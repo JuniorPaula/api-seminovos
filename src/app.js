19704dd18263e87913f92db8465d964425436049
Junior Paula <slucivaldo509@gmail.com>
2022-01-29 12:28:32 -0300

    Configurate eslint, prettier and editorConfig

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
 import 'dotenv/config';
-import express from "express";
+import express from 'express';
 import cors from 'cors';
 
 class App {
-    constructor() {
-        this.app = express();
-        this.middlewares();
-        this.routes();
-    }
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(express.json());
-    }
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
 
-    routes() {
-
-    }
+  routes() {}
 }
 
 export default new App().app;
--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from './app';
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log('Server is running...');
+  console.log('Server is running...');
 });
