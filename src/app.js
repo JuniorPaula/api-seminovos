af83848e4623bfa8864832307e7529236164be74
Junior Paula <slucivaldo509@gmail.com>
2022-01-29 12:24:17 -0300

    initial configurate

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import express from "express";
+import cors from 'cors';
+
+class App {
+    constructor() {
+        this.app = express();
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(express.json());
+    }
+
+    routes() {
+
+    }
+}
+
+export default new App().app;
