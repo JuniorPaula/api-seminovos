beb00e642c5b82c53aad558f9d11ee2c3377ea99
Junior Paula <slucivaldo509@gmail.com>
2022-01-30 10:52:48 -0300

    create Car model

---
+++ src/models/Car.js
@@ -0,0 +1,24 @@
+import mongoose from '../config/database';
+import { Schema } from 'mongoose';
+
+const Car = mongoose.model(
+  'Car',
+  new Schema(
+    {
+      model: { type: String, required: true },
+      brand: { type: String, required: true },
+      color: { type: String, required: true },
+      description: { type: String, required: true },
+      km: { type: Number, required: true },
+      year: { type: Number, required: true },
+      price: { type: Number, required: true },
+      image: { type: Array, required: true },
+      available: { type: Boolean },
+      user: Object,
+      buyer: Object,
+    },
+    { timestamps: true },
+  ),
+);
+
+export default Car;
